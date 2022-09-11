463fe05322c90c4e1d0864aa811498eed846e7e0
Vatsal Gandhi <86202928+Vatsal-01000111@users.noreply.github.com>
2022-09-11 15:29:28 +0530

    Final upload

--- js/addrow.js
+++ js/addrow.js
@@ -1,24 +1,30 @@
 const addbtn = document.getElementById("add_more_fields");
 var list = document.getElementById("speclist");
+var counter = 1;
+var hiden = document.getElementById("no_of_specs");
+    hiden.innerHTML = counter;
+var tempid = "spec1";
 
 addbtn.addEventListener('click',(e)=>{
-
+    counter++;
     e.preventDefault();
     const myli = document.createElement('li');
+    var idspec = "spec"+counter;
+    tempid=idspec;
     myli.innerHTML = `
-                    <input type="text" id="spec" name="specname" value="" placeholder="Specification Name">
-                    <input type="date" id="spec" name="specdate" value="2000-01-01" min="2000-01-01" max="3000-12-31" placeholder="Specification Date">
+                    <input type="text" class="specnamestyle" id="spec1" list="specnames" name="specname" value="" placeholder="Specification Name">
+                    <input type="date" id="${idspec} class="specstyle" name="specdate" value="2000-01-01" min="2000-01-01" max="3000-12-31" placeholder="Specification Date">
                     <br>
-                    <input type="text" id="spec" name="speccompany" value="" placeholder="Company Name">
-                    <input type="text" id="spec" name="specdescription" value="" placeholder="Description">
+                    <input type="text" id=${idspec} class="specstyle" name="speccompany" value="" placeholder="Company Name">
+                    <input type="text" id=${idspec} class="specstyle" name="specdescription" value="" placeholder="Description">
                     <span id="hidethis"><button id="close" onclick="remove()"><img src="../static/x-square.svg" alt=""></button></span>
-                    <input type="text" id="spec" name="specquantity" value="" placeholder="Quantity">
-                    <input type="text" id="spec" name="specamount" value="" placeholder="Amount">
-                    
-    
-                    `;
+                    <input type="text["counter"]" id=${idspec} class="specstyle" name="specquantity" value="" placeholder="Quantity">
+                    <input type="text["counter"]" id=${idspec} class="specstyle" name="specamount" value="" placeholder="Amount">
+                    `;  
     list.appendChild(myli);
-
+    var hiden = document.getElementById("no_of_specs");
+    hiden.innerHTML = counter;
+    
 
 });
 
@@ -26,7 +32,75 @@ function remove(){
 const closespec = document.querySelectorAll('span');
 for(let i=0;i<closespec.length; i++){
     closespec[i].addEventListener('click',()=>{
+        counter--;
         closespec[i].parentElement.style.display = "none";
+        var hiden = document.getElementById("no_of_specs");
+        hiden.innerHTML = counter;
     })
 } 
 }
+
+
+
+let names = [
+    "Printing",
+    "Lamination",
+    "Foiling",
+    "Die Punching",
+    "Back to back pesting",
+    "Handcutting",
+    "Spot U.V.",
+    "Centre Pinning",
+    "Section Sewing Binding",
+    "Perfect Binding",
+    "Strip Gumming",
+    "Varnishing",
+  ];
+  
+  //Sort names in ascending order
+//   function recommend(){
+  
+//   let sortedNames = names.sort();
+//   //reference
+//   let input = document.activeElement;
+//   console.log(input);
+//   //Execute function on keyup
+//   input.addEventListener("keyup", (e) => {
+//     //loop through above array
+//     //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+//     removeElements();
+//     function displayNames(value) {
+//       input.value = value;
+//       removeElements();
+//     }
+//     for (let i of sortedNames) {
+//       //convert input to lowercase and compare with each string
+//       if (
+//         i.toLowerCase().startsWith(input.value.toLowerCase()) &&
+//         input.value != ""
+//       ) {
+//         //create li element
+        
+//         let listItem = document.createElement("li");
+//         //One common class name
+//         listItem.classList.add("list-items");
+//         listItem.style.cursor = "pointer";
+//         listItem.setAttribute("keyup",displayNames(i));
+//         //Display matched part in bold
+//         let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+//         word += i.substr(input.value.length);
+//         //display the value in array
+//         listItem.innerHTML = word;
+//         document.querySelector(".list").appendChild(listItem);
+//       }
+//     }
+//   });
+  
+//   function removeElements() {
+//     //clear all the item
+//     let items = document.querySelectorAll(".list-items");
+//     items.forEach((item) => {
+//       item.remove();
+//     });
+//   }
+// }
---
+++ js/index.js
@@ -0,0 +1,114 @@
+// for index.html
+//---------------------------------------------------------------------------------------------------------------------
+class TableCSVExporter {
+    constructor (table, includeHeaders = true) {
+        this.table = table;
+        this.rows = Array.from(table.querySelectorAll("tr"));
+
+        if (!includeHeaders && this.rows[0].querySelectorAll("th").length) {
+            this.rows.shift();
+        }
+    }
+
+    convertToCSV () {
+        const lines = [];
+        const numCols = this._findLongestRowLength();
+
+        for (const row of this.rows) {
+            let line = "";
+
+            for (let i = 0; i < numCols; i++) {
+                if (row.children[i] !== undefined) {
+                    line += TableCSVExporter.parseCell(row.children[i]);
+                }
+
+                line += (i !== (numCols - 1)) ? "," : "";
+            }
+
+            lines.push(line);
+        }
+
+        return lines.join("\n");
+    }
+
+    _findLongestRowLength () {
+        return this.rows.reduce((l, row) => row.childElementCount > l ? row.childElementCount : l, 0);
+    }
+
+    static parseCell (tableCell) {
+        let parsedValue = tableCell.textContent;
+
+        // Replace all double quotes with two double quotes
+        parsedValue = parsedValue.replace(/"/g, `""`);
+
+        // If value contains comma, new-line or double-quote, enclose in double quotes
+        parsedValue = /[",\n]/.test(parsedValue) ? `"${parsedValue}"` : parsedValue;
+
+        return parsedValue;
+    }
+}
+
+const dataTable = document.getElementById("job_table");
+const expcsv = document.getElementById("downloadcsv");
+
+expcsv.addEventListener("click", () => {
+    const exporter = new TableCSVExporter(dataTable);
+    const csvOutput = exporter.convertToCSV();
+    const csvBlob = new Blob([csvOutput], { type: "text/csv" });
+    const blobUrl = URL.createObjectURL(csvBlob);
+    const anchorElement = document.createElement("a");
+
+    anchorElement.href = blobUrl;
+    anchorElement.download = "jobs.csv";
+    anchorElement.click();
+
+    setTimeout(() => {
+        URL.revokeObjectURL(blobUrl);
+    }, 500);
+});
+
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("job_table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+  function Export() {
+    html2canvas(document.getElementById('job_table'), {
+        onrendered: function (canvas) {
+            var data = canvas.toDataURL();
+            var docDefinition = {
+                content: [{
+                    image: data,
+                    width: 500
+                }]
+            };
+            pdfMake.createPdf(docDefinition).download("Table.pdf");
+        }
+    });
+}
+
+
+// dropdown
+
+function dropFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    
+  }
+  
